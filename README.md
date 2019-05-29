ff40606fa16b68440cd48a0b4f4a411ce728e3db
yixuanqi <yixuan99316@gmail.com>
2019-05-29 02:51:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# This repo:
+This is a personal repository for the resource used in the group project of UBC CPSC304, 2019S1. This project intends to construct a database to manage a storage company and a website which allow employees and customers of the company to do business and manage the storage. Enter the folder, warehouse, to see the source code for the project.
+
+# warehouse:
+This folder contains the code used for the group project.  The index.html is the home page to start with, and the zyxwstorage.sql is the database backing up this website. Other html and php files are source code of websites used in this project. 
+
+# infrastructure:
+Inside the warehouse folder is the infrastructure used for this project. The infrastructures are created to simplify high-level development and unify the style of the website design because this project has four members involved. 
+
+# notes:
+1. The website is now online at practices.isaaczheli.com
+2. The database system used is mySQL. Oracle and other database management systems are also ok to use, but minor modification of the code may be required.
---
+++ warehouse/llogin.php
@@ -0,0 +1,95 @@
+<?php
+    
+    require_once "pdo_constructor.php";
+
+    if (isset($_POST['cancel'])) {
+        header("Location: index.html");
+    }
+
+    $name = "";
+    $pswd = "";
+    $message = false;
+
+    if (isset($_POST['who']) and isset($_POST['password'])) {
+        $name = $_POST['who'];
+        $pswd =  $_POST['password'];
+
+        if ($name == "" || $pswd == "") {
+            $message = "Employee ID and password are required";
+        } else {
+          $sql = "SELECT innerPIN FROM Labourer WHERE employID = '$name'";
+          $stmt = $pdo->prepare($sql);
+          $stmt->execute();
+          $storedpswd = $stmt->fetch();
+          if ($storedpswd['password'] == $pswd) {
+            header("Location: RPS/game.php?name=".urlencode($_POST['who']));
+          } else {
+            $message = "Incorrect employee ID or password";
+          }
+        }
+      }
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Manager Login</title>
+    <link rel="stylesheet" href="./css/form.css">
+    <link rel="stylesheet" href="./css/navbar.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="navbar">
+    <div class="items" id="thebar">
+      <a href="./index.html#home" id="brandlabel">ZYXW STORAGE</a>
+        <a href="./index.html#about">ABOUT</a>
+        <a href="./index.html#contact">CONTACT</a>
+        <a href="./plans.php">PLANS</a>
+        <a  href="./elogin.php">EMPLOYEE</a> 
+        <a  href="./clogin.php">MY ACCOUNT</a> 
+      <a href="javascript:void(0);" class="icon" onclick="mobileExpand()">
+        <i class="fa fa-bars"></i>
+      </a>
+    </div>
+    </div>
+    <div class="form-container">
+    <form method="post" style="width: 25em; background-color: white;">
+      <div class="form-content">
+        <h2 style="color: #002145;">CPSC304 Group 1 Project</h2>
+        <h3 style="color: #002145;">Login to your account:</h3>
+        <p>Employee ID:<br>
+          <input class="long" type="text" name="who">
+        </p>
+        <p>Password:<br>
+          <input type="password" name="password"  style="width: 100%">
+        </p>
+        <?php
+          if ($message != false) {
+            echo "<p style='color: red; font-weight: bold;'>$message</p>";  
+          }
+        ?>
+        <div>
+          <input class="button confirm" type="submit" name="confirm" value="Sign In">
+          <input class="button cancel" type="submit" name="cancel" value="Cancel">
+        </div>
+      </div>
+      <div style="padding: 2em; padding-top:0;">
+      <h4 style="margin-top:0;">OR</h4>
+      <a href="">Forget your account?</a>
+      </div>
+    </form>
+        </div>
+    <script>
+    function mobileExpand() {
+      var x = document.getElementById("thebar");
+      if (x.className === "items") {
+        x.className += " responsive";
+      } else {
+        x.className = "items";
+      }
+    }
+    </script>
+  </body>
+</html>
