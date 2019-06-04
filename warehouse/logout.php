e18bbaf243dd816a61107db99eb948ba63c8f1f5
Zhe Li <lizhe1313@outlook.com>
2019-06-03 22:57:48 -0700

    Add files via upload
    Managers can now logout!

--- warehouse/logout.php
+++ warehouse/logout.php
@@ -2,6 +2,9 @@
     if (isset($_COOKIE['zyxwuser']) and isset($_COOKIE['zyxwpswd'])) {
         setcookie("zyxwuser", "", time() - 3600);
         setcookie("zyxwpswd", "", time() - 3600);
+    } else if (isset($_COOKIE['zyxwmanager']) and isset($_COOKIE['zyxwmpswd'])) {
+        setcookie("zyxwmanager", "", time() - 3600);
+        setcookie("zyxwmpswd", "", time() - 3600);
     }
     header("Location: index.html");
 ?>
