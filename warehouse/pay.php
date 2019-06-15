a070c885b6e8e4c1b16295a6be08d49c8c96c6ed
Yi Xuan Qi <47555540+yixuanQicq@users.noreply.github.com>
2019-06-14 18:09:43 -0700

    update payment page title

--- warehouse/pay.php
+++ warehouse/pay.php
@@ -20,7 +20,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Manager Login</title>
+    <title>Payment</title>
     <link rel="stylesheet" href="./css/form.css">
     <link rel="stylesheet" href="./css/navbar.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
