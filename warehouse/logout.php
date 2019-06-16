2a03f6658f36b06753a7da60bd513758b57d18db
Zhe Li <lizhe1313@outlook.com>
2019-06-16 02:44:35 -0700

    Update logout.php
    The logout.php now delete all the cookies.

--- warehouse/logout.php
+++ warehouse/logout.php
@@ -2,15 +2,22 @@
     if (isset($_COOKIE['zyxwuser']) and isset($_COOKIE['zyxwpswd'])) {
         setcookie("zyxwuser", "", time() - 3600);
         setcookie("zyxwpswd", "", time() - 3600);
-    } else if (isset($_COOKIE['zyxwmanager']) and isset($_COOKIE['zyxwmpswd'])) {
+    } 
+
+    if (isset($_COOKIE['zyxwmanager']) and isset($_COOKIE['zyxwmpswd'])) {
         setcookie("zyxwmanager", "", time() - 3600);
         setcookie("zyxwmpswd", "", time() - 3600);
-    } else if (isset($_COOKIE['zyxwdirector']) and isset($_COOKIE['zyxwdpswd'])) {
+    }
+    
+    if (isset($_COOKIE['zyxwdirector']) and isset($_COOKIE['zyxwdpswd'])) {
         setcookie("zyxwdirector", "", time() - 3600);
         setcookie("zyxwdpswd", "", time() - 3600);
-    } else if (isset($_COOKIE['zyxwworker']) and isset($_COOKIE['zyxwwpswd'])) {
+    }
+    
+    if (isset($_COOKIE['zyxwworker']) and isset($_COOKIE['zyxwwpswd'])) {
         setcookie("zyxwworker", "", time() - 3600);
         setcookie("zyxwwpswd", "", time() - 3600);
     }
+    
     header("Location: index.php");
 ?>
