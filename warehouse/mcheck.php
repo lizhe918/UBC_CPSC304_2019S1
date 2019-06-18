17783ded980b6a236c22dfc722cb292a52f3dfcf
Yi Xuan Qi <47555540+yixuanQicq@users.noreply.github.com>
2019-06-18 09:03:03 -0700

    update check.php with safari calendar

--- warehouse/mcheck.php
+++ warehouse/mcheck.php
@@ -289,6 +289,15 @@ if (isset($_POST) & !empty($_POST)) {
         </a>
     </div>
 </section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://cdn.jsdelivr.net/webshim/1.12.4/extras/modernizr-custom.js"></script>
+<script src="http://cdn.jsdelivr.net/webshim/1.12.4/polyfiller.js"></script>
+<script>
+webshims.setOptions('waitReady', false);
+webshims.setOptions('forms-ext', {type: 'date'});
+webshims.setOptions('forms-ext', {type: 'time'});
+webshims.polyfill('forms forms-ext');
+</script>
 <script>
     function mobileExpandMain() {
         var x = document.getElementById("mainbar");
