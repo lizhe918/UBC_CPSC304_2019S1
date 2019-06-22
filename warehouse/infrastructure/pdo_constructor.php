c66e636157ab9a05c8c38e521e3fdb8a6ccd704a
Zhe Li <lizhe1313@outlook.com>
2019-06-21 19:08:27 -0700

    Update pdo_constructor.php

--- warehouse/infrastructure/pdo_constructor.php
+++ warehouse/infrastructure/pdo_constructor.php
@@ -1,15 +1,9 @@
 <?php
 
 /* 
- * Comment out this line for local development.
- * Uncomment this line when uploading to the server.
- */
-//$pdo=new PDO('mysql:host=mysql.practices.isaaczheli.com;dbname=zyxwstorage','lizhe1313','yxsy0102');
-
-/* 
  * Uncomment this line for local development.
  * Comment out this line when uploading to the server.
  */
-$pdo=new PDO('mysql:host=localhost;dbname=zyxwstorage','lizhe1313','yxsy0102');
+$pdo=new PDO('mysql:host=localhost;dbname=zyxwstorage','username','password');
 
 ?>
